01a3988f2716553b921a02e8e7b4d843c72fab41
MyAngelAyase <hjk21632@163.com>
2015-07-20 19:42:07 +0800

    add skill
    fireout skill not finished

---
+++ game/dota_addons/dopo_empty/scripts/vscripts/AbilityList.lua
@@ -0,0 +1,12 @@
+function FireOut( keys )
+	local caster = keys.caster
+    local target = keys.target
+    local position = keys.target_points
+
+    local damageTable = {victim=target,
+          attacker=caster,
+          damage=100,
+          damage_type=DAMAGE_TYPE_MAGICAL
+                                          }
+    ApplyDamage(damageTable)
+end
