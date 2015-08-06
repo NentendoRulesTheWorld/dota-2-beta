53a65559d171b30d309be660e21b483217624cc7
MyAngelAyase <hjk21632@163.com>
2015-08-06 20:02:09 +0800

    modify debug
    add log type for Vector

--- game/dota_addons/dopo_empty/scripts/vscripts/DebugTools.lua
+++ game/dota_addons/dopo_empty/scripts/vscripts/DebugTools.lua
@@ -1,10 +1,17 @@
 Log = {}
 
+TYPE_BASE = 0
+TYPE_VECTOR = 1
+
 local isDebug = true
 
-function Log:D(info)
+function Log:D(info,type)
 	if isDebug then 
-		print("DOPO_Debug: " .. info)
+		if type == 0 or type == nil
+			print("DOPO_Debug: " .. info)
+		else if type == 1
+			print("DOPO_Debug_Vector: " .. info.x .. " " .. info.y .. " " .. info.z)
+		end
 	end
 end
 
